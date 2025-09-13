af86b91e86fa6ee8960fa1f20c3a7b5b83fb4937
Apsan12 <apsanneupane345@gmail.com>
2025-09-13 16:11:47 +0545

    make the admin dashboard

---
+++ admindashboard/admin/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='text-2xl text-black bg-amber-600 items-center flex-initial'>hey there</p>
+    </div>
+  )
+}
+
+export default App
+
